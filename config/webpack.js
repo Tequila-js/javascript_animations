2c4ddd86eba6c7fda2fb19c722eb8c5765aff45d
Marco Antonio Dominguez <madmzr@gmail.com>
2017-04-18 00:35:50 -0500

    Create webpack configuration, in order to export a function that retrieves the webpack's configuration as a single object.

---
+++ config/webpack.js
@@ -0,0 +1,60 @@
+import path from 'path';
+import webpack from 'webpack';
+
+function config (env = 'development') {
+  let [config, plugins] = [{}, []];
+
+  plugins = [new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js')];
+
+  if (env === 'production') {
+    let webpackEnv, uglify;
+
+    webpackEnv = new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('production')}
+    });
+
+    uglify = new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      compress: {
+        drop_console: true,
+        warnings: false
+      }
+    });
+
+    plugins.push(webpackEnv, uglify);
+  }
+
+  config = {
+    entry: path.join(process.cwd(), '/app/js/main.js'),
+    output: {
+      path: path.join(process.cwd(), '/dist/js'),
+      filename: env === 'production' ? '[name].min.js' : '[name].js',
+    },
+    devtool: env === 'production' ? 'cheap-source-map' : 'source-map',
+    plugins,
+    module: {
+      loaders: [{
+        test: /\.js(x)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0']
+        }
+      }, {
+        test: /\.scss/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }, {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+        loader: 'style-loader!css-loader',
+        exclude: /node_modules/
+      }]
+    },
+    resolve: {
+      extensions: ['.js', '']
+    }
+  };
+
+  return config;
+}
+
+export default config;
--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,25 @@ import {argv} from 'yargs';
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import rename from 'gulp-rename';
-import gulpif from 'gulp-if';
+import gulpIf from 'gulp-if';
 import plumber from 'gulp-plumber';
 import minifycss from 'gulp-minify-css';
 import autoprefixer from 'gulp-autoprefixer';
 import livereload from 'gulp-livereload';
+import webpack from 'gulp-webpack';
 
-const [reload, current] = [argv == 'true', process.cwd()],
-      colors = {
-        green: '\x1b[32m',
-        blue: '\x1b[34m',
-        reset: '\x1b[0m'
-      };
+import config from './config/webpack';
+
+const [reload, env, current] = [argv.reload == 'true', argv.env === 'production' ? 'production' : 'development', process.cwd()],
+  jsConfig = config(env),
+  colors = {
+    green: '\x1b[32m',
+    blue: '\x1b[34m',
+    reset: '\x1b[0m'
+  };
+
+console.log(`${colors.blue}%s${colors.reset}`, `livereload active: ${reload? 'ON' : 'OFF'}`);
+console.log(`${colors.blue}%s${colors.reset}`, `environmemt: ${env}`);
 
 ['dist/js', 'dist/css'].forEach(function checkIfCriticalRouteExist(item) {
   let [routeFolders, route] = [item.split('/'), __dirname];
@@ -31,7 +38,19 @@ const [reload, current] = [argv == 'true', process.cwd()],
   });
 });
 
+function generateJS(done) {
+  gulp.src('./app/js/*.js') 
+    .pipe(plumber())
+    .pipe(webpack(jsConfig))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(gulpIf(reload, livereload()));
+
+  done();
+}
+
 
 gulp.task('watch', function () {
   livereload.listen();
+
+  gulp.watch(['./app/js/*.js'], gulp.parallel(generateJS), done => done());
 });
