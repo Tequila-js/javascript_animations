69bfa605ce10a23cda85f4b4f04e8014f50c83b0
Marco Antonio Dominguez <madmzr@gmail.com>
2017-05-09 01:48:35 -0500

    - Fix eslint rules and update dependency - Fix eslint issues

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import reveal from 'reveal';
 import anime from 'animejs';
 
-const [timeoutTime, animationTime] = [600, 750];
+const [animationTime] = [750];
 
 const defaults = {
   loop: false,
@@ -10,43 +10,45 @@ const defaults = {
   elasticity: () => 500
 };
 
-/* Color Declarations*/
-const blue = {
-  nullOp: 'rgba(57, 194, 215, 0)',
-  middleOp: 'rgba(57, 194, 215, .5)',
-  totalOp: 'rgba(57, 194, 215, 1)'
-};
-
-function  getWindowWidth() {
-  return window.innerWidth || (document.documentElement  || document.getElementsByTagName('body')[0]).clientWidth;
+function getWindowWidth() {
+  return window.innerWidth || (document.documentElement || document.getElementsByTagName('body')[0]).clientWidth;
 }
 
 (function ready(window, document) {
   let $containers = Array.from(document.querySelectorAll('.slides > section')).map(item => $(item)),
     hash = location.hash.replace('#/', ''),
-    [current, numberOfSlides] = [!hash ? 0 : parseInt(hash), $containers.length];
+    [current, numberOfSlides] = [!hash ? 0 : parseInt(hash, 10), $containers.length];
 
   $containers.forEach(function fixTitleFormat($item) {
     let $current = $item.find('.title'),
-      text = ($current.text() || '').split('').map(item => `<span>${item}</span>`);
+      text = ($current.text() || '').split('').map(item => `<span>${item === ' ' ? '&nbsp;' : item}</span>`);
 
     $current.html(text.join(''));
   });
 
   reveal.initialize({
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
     progress: true,
     history: true,
     transition: 'none',
-    center: false,
-    keyboard: false
+    center: false
   });
 
   showContent($containers[current]);
 
   $('.navigate-right').on('click touchstart', nextFn);
   $('.navigate-left').on('click touchstart', prevFn);
+  $(document, window).on('keydown', function(e) {
+    switch (e.key) {
+      case 'ArrowRight':
+        nextFn();
+      case 'ArrowLeft':
+        prevFn();
+        break;
+      default:
+    }
+  });
 
   function nextFn () {
     if (current === numberOfSlides - 1) return;
@@ -61,57 +63,49 @@ function  getWindowWidth() {
     current -= 1;
     showContent($containers[current], 200, current);
   }
-})(window, document);
+}(window, document));
 
 function showContent($element, current = 0) {
-  let [title, content, titleSize] = [$element.find('h1 span').toArray(), $element.find('.content').toArray(), 0];
+  let [title, content] = [$element.find('h1 span').toArray(), $element.find('.content').toArray()];
 
   if ([1, 2].indexOf(current)) {
     let glasses = $element.find('.content .glasses').toArray();
     animateGlasses(glasses);
   }
 
-  titleSize = displayTitle(title) + 65;
+  displayTitle(title);
   displayContent(content);
-
-  $element.find('h1').height(titleSize);
 }
 
 function displayTitle (elements = [], time = 200) {
-  let [positionWidth, width, availableWidth, xVal, yVal]= [35, getWindowWidth(), 0, 0, 0];
-  availableWidth = width * .25;
+  let [positionWidth, width, availableWidth, yVal, total]= [35, getWindowWidth(), 0, 0, $(window).width()];
 
   if (!(elements instanceof Array)) return;
+  total = $(window).width() / elements.length;
 
   elements.forEach(function (item, i) {
-    let [config, yDist] = [null, i * positionWidth * 3];
+    let [config, currentVal, animation] = [null, i * total, 2 * i * time];
 
     config = {
       targets: elements[i],
       left: [
-        {value: 0, duration: 0},
-        {value: xVal, duration: i * time}
+        {value: (Math.random() * 10) > 5? currentVal: -currentVal, duration: 0},
+        {value: 0, duration: animationTime * 5},
       ],
-      translateY: [
-        {value: i % 2 === 0? - yDist: yDist, duration: 0},
-        {value: yVal, duration: animationTime}
+      top: [
+        {value: (Math.random() * 10) > 5? currentVal: -currentVal, duration: 0},
+        {value: 0, duration: animationTime * 5}
       ],
       opacity: [
         {value: 0, duration: 0},
-        {value: 1, duration: animationTime / 2}
-      ],
+        {value: 1, duration: animationTime * 2}
+      ],/*
       color: [
-        {value: blue.middleOp, duration: animationTime / 2},
-        {value: blue.totalOp, duration: animationTime }
-      ]
+        {value: '#39c2d7', duration: animationTime / 2},
+        {value: '#fff', duration: animationTime }
+      ],*/
+      elasticity: () => 500
     };
-
-    if (xVal >= availableWidth && item.innerText === '') {
-      [xVal, yVal] = [0, yVal + 60];
-    } else {
-      xVal += item.innerText === ''? 15 : item.offsetWidth;
-    }
-
     anime(Object.assign({}, config, defaults));
   });
 
