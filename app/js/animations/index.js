addb476afa5930fe1651a45fd2d970309ee20f82
Marco Antonio Dominguez <madmzr@gmail.com>
2017-05-14 20:08:36 -0500

    Include animacte-social-media

--- app/js/animations/index.js
+++ app/js/animations/index.js
@@ -1,3 +1,4 @@
 export displayTitle from './display-title';
 export displayContent from './display-content';
 export animateGlasses from './animate-glasses';
+export animateSocialMedia from './animate-social-media';
